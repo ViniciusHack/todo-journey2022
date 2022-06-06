b711f84ca5faf590f68edf16e1793dba06f8518e
Vinícius Hack <viniciusvwh1441@gmail.com>
2022-06-05 21:21:59 -0300

    feat: initial settings and Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+
+import TodoLogo from '../assets/todo-logo.svg';
+import styles from './Header.module.css';
+
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={TodoLogo} />
+    </header>
+  )
+}
